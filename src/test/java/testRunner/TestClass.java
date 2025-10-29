3831ee6dad2a69162ed572c920a6d032772c5bbf
“SDETAnant” <anantdokde7414@gmail.com>
2025-10-29 17:51:18 +0530

    TestClass added

---
+++ src/test/java/testRunner/TestClass.java
@@ -0,0 +1,17 @@
+package testRunner;
+
+public class TestClass {
+	@FunctionalInterface
+	interface MyFunctionalInterface 
+	{
+	    void sayHello();   // one abstract method
+	   
+	}
+
+	public static void main(String[] args) 
+	{
+	 	
+    
+	}
+
+}
